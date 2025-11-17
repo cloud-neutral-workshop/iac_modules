6b1e04f5da3d4a45169fce59ab8aae729b598dcf
Haitao Pan <manbuzhe2008@gmail.com>
2025-11-17 16:25:32 +0800

    refactor(msk): adopt new storage_info schema for MSK EBS configuration

--- iac-template/terraform-standard/modules/msk/main.tf
+++ iac-template/terraform-standard/modules/msk/main.tf
@@ -4,10 +4,15 @@ resource "aws_msk_cluster" "this" {
   number_of_broker_nodes = var.number_of_broker_nodes
 
   broker_node_group_info {
-    instance_type    = var.instance_type
-    client_subnets   = var.subnet_ids
-    ebs_volume_size  = var.volume_size
-    security_groups  = []
+    instance_type  = var.instance_type
+    client_subnets = var.subnet_ids
+    security_groups = []
+
+    storage_info {
+      ebs_storage_info {
+        volume_size = var.volume_size
+      }
+    }
   }
 
   tags = merge(var.tags, {
